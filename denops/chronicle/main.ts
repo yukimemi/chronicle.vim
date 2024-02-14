040337860c811346cde48a24a8333c8e7857e290
GitHub <noreply@github.com>
2024-02-14 06:05:32 +0000

    chore(deps): Update deno dependencies

--- denops/chronicle/main.ts
+++ denops/chronicle/main.ts
@@ -15,7 +15,7 @@ import { batch } from "https://deno.land/x/denops_std@v6.0.1/batch/mod.ts";
 import dir from "https://deno.land/x/dir@1.5.2/mod.ts";
 import type { Denops } from "https://deno.land/x/denops_std@v6.0.1/mod.ts";
 import { Semaphore } from "https://deno.land/x/async@v2.1.0/mod.ts";
-import { assert, ensure, is } from "https://deno.land/x/unknownutil@v3.16.0/mod.ts";
+import { assert, ensure, is } from "https://deno.land/x/unknownutil@v3.16.1/mod.ts";
 
 let debug = false;
 let enable = true;
